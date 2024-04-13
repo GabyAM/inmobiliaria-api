301d587b8a57374a7a763ab983097881cb07ec23
GabyAM <gdgabyam@gmail.com>
2024-04-13 12:53:26 -0300

    añade función para crear propiedad
    Esta función implementa validaciones de campos con la librería 'respect/validation'. El campo 'disponible' siempre devuelve el error de que está vacio, así que lo tengo que dejar como opcional por el momento.
    
    Una vez que los campos fueron validados, se comprueba que los campos de ids correspondan a un elemento en sus respectivas tablas (esto se podría realizar con una validación custom pero no supe como), y luego se construye la consulta de sql con los valores existentes para crear la propiedad.

--- slim/index.php
+++ slim/index.php
@@ -3,6 +3,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Respect\Validation\Validator as v;
 
 require __DIR__ . '/vendor/autoload.php';
 include './validaciones/localidad.php';
@@ -282,6 +283,124 @@ $app->get('/propiedades', function (Request $request, Response $response) {
         $data = ['status' => 'success', 'results' => $results];
 
         $response->getBody()->write(json_encode($data));
+        return $response->withStatus(201);
+    } catch (\Exception $e) {
+        $response->getBody()->write(
+            json_encode([
+                'status' => 'failure',
+                'error' => $e->getMessage(),
+            ])
+        );
+        return $response->withStatus(500);
+    }
+});
+
+$app->post('/propiedades', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+    $validaciones = [
+        'domicilio' => v::notEmpty()->stringType(),
+        'localidad_id' => v::notEmpty()->intVal(),
+        'cantidad_habitaciones' => v::optional(v::intVal()),
+        'cantidad_banios' => v::optional(v::intVal()),
+        'cochera' => v::optional(v::boolType()),
+        'cantidad_huespedes' => v::notEmpty()->intVal(),
+        'fecha_inicio_disponibilidad' => v::notEmpty()->date(),
+        'cantidad_dias' => v::notEmpty()->intVal(),
+        'disponible' => v::optional(v::boolType()), //Lo hago opcional por el momento porque por algún motivo no se valida correctamente
+        'valor_noche' => v::notEmpty()->intVal(),
+        'tipo_propiedad_id' => v::notEmpty()->intVal(),
+        'imagen' => v::optional(v::stringType()),
+        'tipo_imagen' => v::optional(v::regex('/jpg|jpeg|png/')),
+    ];
+
+    $errores = [];
+    foreach ($validaciones as $campo => $regla) {
+        $valor = isset($data[$campo]) ? $data[$campo] : null;
+        try {
+            $regla->assert($valor);
+        } catch (\Respect\Validation\Exceptions\NestedValidationException $e) {
+            $errores[$campo] = $e->getMessages([
+                'notEmpty' => 'Este campo es requerido',
+                'stringType' => 'Este campo debe ser de tipo string',
+                'intVal' => 'Este campo debe ser un entero',
+                'boolType' => 'Este campo debe ser un booleano',
+                'date' =>
+                    'Este campo debe ser una fecha en el formato 2024-04-12',
+                'regex' => 'Este campo no está en el formato correcto',
+            ]);
+        }
+    }
+
+    if (!empty($errores)) {
+        $response->getBody()->write(json_encode(['errors' => $errores]));
+        return $response->withStatus(400);
+    }
+
+    try {
+        $pdo = createConnection();
+
+        $localidadId = $data['localidad_id'];
+        $sql = 'SELECT * FROM localidades WHERE id = :id';
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':id', $localidadId);
+        $query->execute();
+        if ($query->rowCount() == 0) {
+            $response->getBody()->write(
+                json_encode([
+                    'status' => 'failure',
+                    'error' => 'No existe una localidad con el ID provisto',
+                ])
+            );
+            return $response->withStatus(400);
+        }
+
+        $tipoPropiedadId = $data['tipo_propiedad_id'];
+        $sql = 'SELECT * FROM tipo_propiedades WHERE id = :id';
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':id', $tipoPropiedadId);
+        $query->execute();
+        if ($query->rowCount() == 0) {
+            $response->getBody()->write(
+                json_encode([
+                    'status' => 'failure',
+                    'error' =>
+                        'No existe una tipo de propiedad con el ID provisto',
+                ])
+            );
+            return $response->withStatus(400);
+        }
+
+        $stringCampos = '';
+        $stringValores = '';
+        $i = 0;
+        foreach ($data as $key => $value) {
+            $stringCampos .= $key;
+            if (is_bool($value)) {
+                $stringValores .= $value ? 'true' : 'false';
+            } elseif (is_string($value)) {
+                $stringValores .= '"' . $value . '"';
+            } else {
+                $stringValores .= $value;
+            }
+            if ($i < count($data) - 1) {
+                $stringCampos .= ', ';
+                $stringValores .= ', ';
+            }
+            $i++;
+        }
+        $sql =
+            'INSERT INTO propiedades (' .
+            $stringCampos .
+            ') VALUES (' .
+            $stringValores .
+            ')';
+        $query = $pdo->query($sql);
+        $response->getBody()->write(
+            json_encode([
+                'status' => 'success',
+                'message' => 'Propiedad creada',
+            ])
+        );
         return $response->withStatus(201);
     } catch (\Exception $e) {
         $response->getBody()->write(
